01fc4d1399f8e11bdb8e3a8e47d3c72498c9414c
EnesBozdemir <76599627+EnesBozdemir@users.noreply.github.com>
2023-03-18 15:08:33 +0300

    product-filter-app

---
+++ src/components/product/ProductList.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import Categories from '../categories/Categories'
+import Search from '../search/Search'
+import Product from './Product'
+import "./ProductList.css"
+import {products as productData} from "../../products-data"
+
+
+const ProductList = () => {
+  const [products, setProducts] = useState(productData)
+  const [search, setSearch] = useState("")
+  const [filteredProducts, setFilteredProducts] = useState([])
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  useEffect(() => {
+    setFilteredProducts(
+      products.filter((product) => product.title.toLowerCase().includes(search.toLowerCase()))
+    )
+  }, [search, products])
+
+  return (
+    <>
+     <div className="header">
+        <header className="container">
+            <h1 className='--color-white --text-center'>
+              <span className='--color-danger'>Product</span> Filter
+            </h1>
+            <div className='--flex-between --flex-dir-column --py'>
+              <Search inputValue={search} onInputChange={handleSearch}/>
+              <Categories/>
+            </div>
+        </header>
+        <div className="product-container">
+          <div className="products container --grid-25 --py2">
+            {filteredProducts.length === 0 ? (
+              <h3>No product found!!!</h3>
+            ) : (
+              filteredProducts.map((product) =>{
+                const {id, title, img, price} =product
+                return(
+                  <div key={id}>
+                    <Product title={title} img={img} price={price}/>
+                  </div>
+                )
+              }
+            )
+            )}
+          </div>
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default ProductList
